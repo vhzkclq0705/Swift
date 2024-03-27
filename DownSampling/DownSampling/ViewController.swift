9a5b1b60f167ee907136f409e4a4f355d80da9c2
oh_jun <vhzkclq0705@naver.com>
2024-03-27 19:58:43 +0900

    [ADD] 다운샘플링 프로젝트 생성

---
+++ DownSampling/DownSampling/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DownSampling
+//
+//  Created by 권오준 on 2024-03-27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
