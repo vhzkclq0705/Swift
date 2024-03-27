2af7e7f6ad23edb9e8480061cf1d79f129dfaa94
oh_jun <vhzkclq0705@naver.com>
2024-03-27 20:30:01 +0900

    [ADD] 베이스 구현

--- DownSampling/DownSampling/ViewController.swift
+++ DownSampling/DownSampling/ViewController.swift
@@ -8,12 +8,66 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - UI
+    
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        
+        return imageView
+    }()
+    
+    // MARK: - Properties
+    
+    private let url = URL(string: "https://developer.apple.com/home/images/hero-wwdc24/phase1-awre32/b-wwdc24-hero-large-phase1_2x.webp")!
+    
+    // MARK: - Life cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureUI()
+        configureLayout()
+        fetchImage()
     }
-
-
+    
+    // MARK: - Configure
+    
+    private func configureUI() {
+        view.addSubview(imageView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private func configureLayout() {
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            imageView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
+            imageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            imageView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20)
+        ])
+    }
+    
+    // MARK: - Functions
+    
+    private func fetchImage() {
+        DispatchQueue.global().async { [weak self] in
+            guard let self = self,
+                  let imageData = try? Data(contentsOf: self.url),
+                  let image = UIImage(data: imageData) else {
+                print("This URL is not available.")
+                return
+            }
+            
+            self.displayInfoOfImage(imageData, image)
+            DispatchQueue.main.async {
+                self.imageView.image = image
+            }
+        }
+    }
+    
+    private func displayInfoOfImage(_ data: Data, _ image: UIImage) {
+        print("Data size: \(data.count) bytes")
+        print("Image width: \(image.size.width), height: \(image.size.height)")
+    }
+    
 }
-
