2acbe2a85f090fca637793f4939340cd3707e8b0
oh_jun <vhzkclq0705@naver.com>
2024-03-28 00:10:34 +0900

    [MOD] 이미지 비율 수정

--- DownSampling/DownSampling/DownSampler.swift
+++ DownSampling/DownSampling/DownSampler.swift
@@ -27,7 +27,6 @@ class DownSampler {
                 return
             }
             
-            print("Successed to download image.")
             completion(data)
         }.resume()
     }
@@ -39,68 +38,77 @@ class DownSampler {
             return
         }
         print("Data size: \(data.count) bytes")
+        print("Image size \(image.size)")
     }
     
-    /// Downsample with UIImage
-    func downsampleWithUIImage(
-        _ scale: CGFloat,
-        completion: @escaping (UIImage?) -> Void)
-    {
+    /// Fetch original image
+    func fetchOriginalImage(completion: @escaping (UIImage?) -> Void) {
         fetchImage() { [weak self] data in
             let start = Date()
-            guard let image = UIImage(data: data) else {
+            guard let originalImage = UIImage(data: data) else {
                 print("Failed to create UIImage.")
                 completion(nil)
                 return
             }
             
-            let width = image.size.width * scale
-            let height = image.size.height * scale
-            let size = CGSize(width: width, height: height)
-            let renderer = UIGraphicsImageRenderer(size: size)
+            let end = Date()
+            print("_____downloadOriginalImage_____")
+            print("Time: \(end.timeIntervalSince(start))")
+            self?.displayInfoOfImage(originalImage)
+            
+            completion(originalImage)
+        }
+    }
+    
+    /// Downsample with UIGraphicsImageRenderer
+    func downsampleWithUIRenderer(_ size: CGSize, completion: @escaping (UIImage?) -> Void) {
+        fetchImage() { [weak self] data in
+            let start = Date()
+            guard let self = self,
+                  let image = UIImage(data: data) else {
+                print("Failed to create UIImage.")
+                completion(nil)
+                return
+            }
+            
+            let originalSize = image.size
+            let ratio = originalSize.width / originalSize.height
+            let width = size.width
+            let height = width / ratio
+            let targetSize = originalSize.width > originalSize.height ?
+                CGSize(width: width, height: height) :
+                CGSize(width: width * ratio, height: width)
+            
+            let renderer = UIGraphicsImageRenderer(size: targetSize)
             let downsampledImage = renderer.image { context in
-                image.draw(in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
+                image.draw(in: CGRect(origin: .zero, size: targetSize))
             }
             
             let end = Date()
-            let interval = end.timeIntervalSince(start)
-            
-            print("_____resizedImageWithUIRenderer_____")
+            print("_____downsampledImageWithUIRenderer_____")
             print("Time: \(end.timeIntervalSince(start))")
-            self?.displayInfoOfImage(downsampledImage)
+            self.displayInfoOfImage(downsampledImage)
             
             completion(downsampledImage)
         }
     }
     
     /// Downsample with ImageIO
-    func downsampleWithImageIO(
-        _ size: CGSize,
-        _ scale: CGFloat,
-        completion: @escaping (UIImage?) -> Void)
-    {
+    func downsampleWithImageIO(_ size: CGSize, completion: @escaping (UIImage?) -> Void) {
         fetchImage() { [weak self] data in
             let start = Date()
-            guard let imageSource = CGImageSourceCreateWithData(data as CFData, nil) else {
+            guard let self = self,
+                  let imageSource = CGImageSourceCreateWithData(data as CFData, nil) else {
                 print("Failed to create image source.")
                 completion(nil)
                 return
             }
-            
-            let options1: [NSString: Any] = [kCGImageSourceShouldCache: false]
-            guard let properties = CGImageSourceCopyPropertiesAtIndex(imageSource, 0, options1 as CFDictionary) as? [String: Any],
-                  let width = properties[kCGImagePropertyPixelWidth as String] as? CGFloat,
-                  let height = properties[kCGImagePropertyHeight as String] as? CGFloat else {
-                print("Failed to get image size")
-                completion(nil)
-                return
-            }
-            
-            let options2: [NSString: Any] = [
-                kCGImageSourceThumbnailMaxPixelSize: max(width, height) * scale,
+
+            let options: [NSString: Any] = [
+                kCGImageSourceThumbnailMaxPixelSize: max(size.width, size.height),
                 kCGImageSourceCreateThumbnailFromImageAlways: true
             ]
-            guard let downsampledImage = CGImageSourceCreateThumbnailAtIndex(imageSource, 0, options2 as CFDictionary) else {
+            guard let downsampledImage = CGImageSourceCreateThumbnailAtIndex(imageSource, 0, options as CFDictionary) else {
                 print("Failed to downsample image source.")
                 completion(nil)
                 return
@@ -108,11 +116,9 @@ class DownSampler {
             
             let image = UIImage(cgImage: downsampledImage)
             let end = Date()
-            let interval = end.timeIntervalSince(start)
-            
-            print("_____resizedImageWithUIRenderer_____")
+            print("_____downsampledImageWithImageIO_____")
             print("Time: \(end.timeIntervalSince(start))")
-            self?.displayInfoOfImage(image)
+            self.displayInfoOfImage(image)
             
             completion(image)
         }
--- DownSampling/DownSampling/ViewController.swift
+++ DownSampling/DownSampling/ViewController.swift
@@ -9,41 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    // MARK: - UI
+    // MARK:  UI
     
-    private lazy var imageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.contentMode = .scaleAspectFit
-        
-        return imageView
-    }()
+    private lazy var imageView = UIImageView()
     
-    private lazy var fetchButton: UIButton = {
+    private lazy var originalButton: UIButton = {
         let button = UIButton()
-        button.setTitle("원본 다운로드", for: .normal)
-        button.tintColor = .green
+        button.setTitle("원본", for: .normal)
+        button.backgroundColor = .red
         
         return button
     }()
     
     private lazy var downsampleButton1: UIButton = {
         let button = UIButton()
-        button.setTitle("원본 다운로드", for: .normal)
-        button.tintColor = .green
-        
+        button.setTitle("UIImage", for: .normal)
+        button.backgroundColor = .green
+
         return button
     }()
     
     private lazy var downsampleButton2: UIButton = {
         let button = UIButton()
-        button.setTitle("원본 다운로드", for: .normal)
-        button.tintColor = .green
+        button.setTitle("ImageIO", for: .normal)
+        button.backgroundColor = .blue
         
         return button
     }()
     
+    // MARK: Properties
     
-    // MARK: - Life cycle
+    private let downsampler = DownSampler()
+    private lazy var size = imageView.frame.size
+    
+    // MARK:  Life cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -51,24 +50,69 @@ class ViewController: UIViewController {
         configureLayout()
     }
     
-    // MARK: - Configure
+    // MARK:  Configure
     
     private func configureUI() {
-        view.addSubview(imageView)
-        imageView.translatesAutoresizingMaskIntoConstraints = false
+        [imageView, originalButton, downsampleButton1, downsampleButton2]
+            .forEach {
+                view.addSubview($0)
+                $0.translatesAutoresizingMaskIntoConstraints = false
+            }
+        
+        [originalButton, downsampleButton1, downsampleButton2]
+            .forEach {
+                $0.addTarget(self, action: #selector(didTapButton(_:)), for: .touchUpInside)
+            }
     }
     
     private func configureLayout() {
         NSLayoutConstraint.activate([
             imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
-            imageView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
+            imageView.bottomAnchor.constraint(equalTo: view.centerYAnchor, constant: -20),
             imageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
-            imageView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20)
+            imageView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            
+            originalButton.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 20),
+            originalButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 50),
+            originalButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -50),
+            originalButton.heightAnchor.constraint(equalToConstant: 50),
+            
+            downsampleButton1.topAnchor.constraint(equalTo: originalButton.bottomAnchor, constant: 20),
+            downsampleButton1.leadingAnchor.constraint(equalTo: originalButton.leadingAnchor),
+            downsampleButton1.trailingAnchor.constraint(equalTo: originalButton.trailingAnchor),
+            downsampleButton1.heightAnchor.constraint(equalToConstant: 50),
+            
+            downsampleButton2.topAnchor.constraint(equalTo: downsampleButton1.bottomAnchor, constant: 20),
+            downsampleButton2.leadingAnchor.constraint(equalTo: originalButton.leadingAnchor),
+            downsampleButton2.trailingAnchor.constraint(equalTo: originalButton.trailingAnchor),
+            downsampleButton2.heightAnchor.constraint(equalToConstant: 50),
         ])
     }
     
-    // MARK: - Functions
+    // MARK:  Functions
     
-
-    
+    @objc func didTapButton(_ sender: UIButton) {
+        switch sender {
+        case originalButton:
+            downsampler.fetchOriginalImage() { [weak self] image in
+                DispatchQueue.main.async {
+                    self?.imageView.image = image
+                }
+            }
+        case downsampleButton1:
+            downsampler.downsampleWithUIRenderer(size) { [weak self] image in
+                DispatchQueue.main.async {
+                    self?.imageView.image = image
+                }
+            }
+        case downsampleButton2:
+            downsampler.downsampleWithImageIO(size) { [weak self] image in
+                DispatchQueue.main.async {
+                    self?.imageView.image = image
+                }
+            }
+        default:
+            return
+        }
+    }
 }
