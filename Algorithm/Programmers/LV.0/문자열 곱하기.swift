70b6ad70263da23856392036c6a4cdae277208d8
oh_jun <vhzkclq0705@naver.com>
2024-06-17 16:54:16 +0900

    문자열 곱하기

---
+++ Algorithm/Programmers/LV.0/문자열 곱하기.swift
@@ -0,0 +1,14 @@
+//
+//  문자열 곱하기.swift
+//  Algorithm
+//
+//  Created by 권오준 on 2024-06-17.
+//
+
+import Foundation
+
+func 문자열곱하기() {
+    func solution(_ my_string:String, _ k:Int) -> String {
+        return String(repeating: my_string, count: k)
+    }
+}
