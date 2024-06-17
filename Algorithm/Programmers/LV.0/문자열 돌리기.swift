1fccb79ddd66094992c7902bebbdf936b19f22c1
oh_jun <vhzkclq0705@naver.com>
2024-06-17 15:58:14 +0900

    문자열 돌리기

---
+++ Algorithm/Programmers/LV.0/문자열 돌리기.swift
@@ -0,0 +1,12 @@
+//
+//  문자열 돌리기.swift
+//  Algorithm
+//
+//  Created by 권오준 on 2024-06-17.
+//
+
+import Foundation
+
+func 문자열돌리기() {
+    print(readLine()!.map { String($0) }.joined(separator: "\n"))
+}
