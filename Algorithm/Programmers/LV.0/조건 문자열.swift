6534747e2ae73ed9ebd1c8e3e3c9ed5d2cd0dbc3
oh_jun <vhzkclq0705@naver.com>
2024-06-18 16:17:31 +0900

    조건 문자열

--- Algorithm/Programmers/LV.0/조건 문자열.swift
+++ Algorithm/Programmers/LV.0/조건 문자열.swift
@@ -9,12 +9,12 @@ import Foundation
 
 func 조건문자열() {
     func solution(_ ineq:String, _ eq:String, _ n:Int, _ m:Int) -> Int {
-        if ineq == "<" {
-            if eq == "=" { return n <= m ? 1 : 0 }
-            else { return n < m ? 1 : 0 }
-        } else {
-            if eq == "=" { return n >= m ? 1 : 0 }
-            else { return n > m ? 1 : 0 }
+        switch (ineq + eq) {
+        case ">=": return n >= m ? 1 : 0
+        case ">!": return n > m ? 1 : 0
+        case "<=": return n <= m ? 1 : 0
+        case "<!": return n < m ? 1 : 0
+        default: return 0
         }
     }
 }
