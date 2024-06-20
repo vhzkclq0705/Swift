40e3d11df169794738fc60fab93008b72a2f425d
oh_jun <vhzkclq0705@naver.com>
2024-06-20 17:00:51 +0900

    카운트 업

---
+++ Algorithm/Programmers/LV.0/카운트 업.swift
@@ -0,0 +1,14 @@
+//
+//  카운트 업.swift
+//  Algorithm
+//
+//  Created by 권오준 on 2024-06-20.
+//
+
+import Foundation
+
+func 카운트업() {
+    func solution(_ start_num:Int, _ end_num:Int) -> [Int] {
+        return Array(start_num...end_num)
+    }
+}
