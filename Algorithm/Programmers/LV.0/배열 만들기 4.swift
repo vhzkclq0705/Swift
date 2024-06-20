6bceefd21324afa5fee47917205d6f98ce5d916a
oh_jun <vhzkclq0705@naver.com>
2024-06-20 17:16:20 +0900

    배열 만들기 4

---
+++ Algorithm/Programmers/LV.0/배열 만들기 4.swift
@@ -0,0 +1,31 @@
+//
+//  배열 만들기 4.swift
+//  Algorithm
+//
+//  Created by 권오준 on 2024-06-20.
+//
+
+import Foundation
+
+func 배열만들기4() {
+    func solution(_ arr:[Int]) -> [Int] {
+        var stk = [Int]()
+        var i = 0
+        
+        while i < arr.count {
+            if stk.isEmpty {
+                stk.append(arr[i])
+                i += 1
+            } else {
+                if stk.last! < arr[i] {
+                    stk.append(arr[i])
+                    i += 1
+                } else {
+                    stk.popLast()
+                }
+            }
+        }
+        
+        return stk
+    }
+}
