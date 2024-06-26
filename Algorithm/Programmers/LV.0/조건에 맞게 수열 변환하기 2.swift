749ad91ba98011286b6cf6d11177a00c9b71eaf8
oh_jun <vhzkclq0705@naver.com>
2024-06-26 14:50:02 +0900

    조건에 맞게 수열 변환하기 2

---
+++ Algorithm/Programmers/LV.0/조건에 맞게 수열 변환하기 2.swift
@@ -0,0 +1,27 @@
+//
+//  조건에 맞게 수열 변환하기 2.swift
+//  Algorithm
+//
+//  Created by 권오준 on 2024-06-26.
+//
+
+import Foundation
+
+func solution(_ arr:[Int]) -> Int {
+    var arr = arr
+    var x = 0
+    
+    while true {
+        let tmp: [Int] = arr.map {
+            if $0 >= 50 && $0 % 2 == 0 { return $0 / 2 }
+            else if $0 < 50 && $0 % 2 == 1 { return $0 * 2 + 1 }
+            return $0
+        }
+        if arr == tmp { break }
+        arr = tmp
+        
+        x += 1
+    }
+    
+    return x
+}
