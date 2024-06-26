7abf3a8c9986a74b125adba970d7f2ac94a71454
oh_jun <vhzkclq0705@naver.com>
2024-06-26 15:38:30 +0900

    조건에 맞게 수열 변환하기 2 수정

--- Algorithm/Programmers/LV.0/조건에 맞게 수열 변환하기 2.swift
+++ Algorithm/Programmers/LV.0/조건에 맞게 수열 변환하기 2.swift
@@ -7,21 +7,23 @@
 
 import Foundation
 
-func solution(_ arr:[Int]) -> Int {
-    var arr = arr
-    var x = 0
-    
-    while true {
-        let tmp: [Int] = arr.map {
-            if $0 >= 50 && $0 % 2 == 0 { return $0 / 2 }
-            else if $0 < 50 && $0 % 2 == 1 { return $0 * 2 + 1 }
-            return $0
-        }
-        if arr == tmp { break }
-        arr = tmp
+func 조건에맞게수열변환하기2() {
+    func solution(_ arr:[Int]) -> Int {
+        var arr = arr
+        var x = 0
         
-        x += 1
+        while true {
+            let tmp: [Int] = arr.map {
+                if $0 >= 50 && $0 % 2 == 0 { return $0 / 2 }
+                else if $0 < 50 && $0 % 2 == 1 { return $0 * 2 + 1 }
+                return $0
+            }
+            if arr == tmp { break }
+            arr = tmp
+            
+            x += 1
+        }
+        
+        return x
     }
-    
-    return x
 }
