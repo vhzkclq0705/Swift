c9973dd655ef2076848e183511e3be2e18537304
oh_jun <vhzkclq0705@naver.com>
2024-06-26 17:58:35 +0900

    공백으로 구분하기 2

---
+++ Algorithm/Programmers/LV.0/공백으로 구분하기 2.swift
@@ -0,0 +1,14 @@
+//
+//  공백으로 구분하기 2.swift
+//  Algorithm
+//
+//  Created by 권오준 on 2024-06-26.
+//
+
+import Foundation
+
+func 공백으로구분하기2() {
+    func solution(_ my_string:String) -> [String] {
+        return my_string.split(whereSeparator: { $0.isWhitespace }).map { String($0) }
+    }
+}
