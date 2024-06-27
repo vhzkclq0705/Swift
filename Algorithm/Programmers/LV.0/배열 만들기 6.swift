422f326e10440d952c174a380c7ff0800aacaad8
oh_jun <vhzkclq0705@naver.com>
2024-06-27 20:46:04 +0900

    배열 만들기 6

---
+++ Algorithm/Programmers/LV.0/배열 만들기 6.swift
@@ -0,0 +1,19 @@
+//
+//  배열 만들기 6.swift
+//  Algorithm
+//
+//  Created by 권오준 on 2024-06-27.
+//
+
+import Foundation
+
+func 배열만들기6() {
+    func solution(_ arr:[Int]) -> [Int] {
+        let arr = arr.reduce(into: (0, [Int]())) {
+            if $0.1.isEmpty || (!$0.1.isEmpty && $0.1.last! != $1) { $0.1.append($1)}
+            else { $0.1.popLast()}
+            $0.0 += 1
+        }
+        return arr.1.isEmpty ? [-1] : arr.1
+    }
+}
